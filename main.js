77bf330408b41198a048917333fe77999d92a70c
Ronny Bezerra <arbo09427@gmail.com>
2025-06-04 20:26:25 -0300

    Addition of items array on localStorage and creation of verifyLocalStorageItems functions to verify if there are already saved items and update the array

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const items = []
+let items = []
 
 function addItem() {
     const itemName = document.querySelector("#item").value
@@ -38,6 +38,8 @@ function showItemsList() {
             </div>
         `
     })
+
+    localStorage.setItem("items", JSON.stringify(items))
 }
 
 function removeItem(itemName) {
@@ -66,3 +68,14 @@ function checkItem(itemName) {
     item.checked = !item.checked
     showItemsList()
 }
+
+function verifyLocalStorageItems() {
+    const localStorageItems = localStorage.getItem("items")
+
+    if (localStorageItems) {
+        items = JSON.parse(localStorageItems)
+        showItemsList()
+    }
+}
+
+verifyLocalStorageItems()
